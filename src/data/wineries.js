caadad7ce2fd1f621bcfc088bf51bbd6dfedff71
ronen raz <journey@blixa.org>
2024-10-07 06:08:23 -0700

    syntax error

--- src/data/wineries.js
+++ src/data/wineries.js
@@ -1,81 +1,91 @@
-const wineries -[
+const wineries = [
   {
-    "name": "Carol Shelton Wines",
-    "address": "3354-B Coffey Lane, Santa Rosa",
-    "wines": ["Rhone Style Red Blend", "Grenache", "Mourvèdre"],
-    "food": "Yellow Curry with Roasted Winter Vegetables",
-    "homepage": "https://carolshelton.com"
+    name: "Carol Shelton Wines",
+    address: "3354-B Coffey Lane, Santa Rosa",
+    wines: ["Rhone Style Red Blend", "Grenache", "Mourvèdre"],
+    food: "Yellow Curry with Roasted Winter Vegetables",
+    homepage: "https://carolshelton.com",
   },
   {
-    "name": "Notre Vue Estate",
-    "address": "11010 Estate Lane, Windsor",
-    "wines": ["Bordeaux Style Blends", "Rhone Style Blends"],
-    "food": "Renee's Famous Roasted Garlic Mashtinis",
-    "homepage": "https://notrevueestate.com"
+    name: "Notre Vue Estate",
+    address: "11010 Estate Lane, Windsor",
+    wines: ["Bordeaux Style Blends", "Rhone Style Blends"],
+    food: "Renee's Famous Roasted Garlic Mashtinis",
+    homepage: "https://notrevueestate.com",
   },
   {
-    "name": "Spicy Vines",
-    "address": "441 Healdsburg Avenue, Healdsburg",
-    "wines": ["Syrah"],
-    "food": "Venison Chili with a Blueberry Reduction",
-    "homepage": "https://spicyvines.com"
+    name: "Spicy Vines",
+    address: "441 Healdsburg Avenue, Healdsburg",
+    wines: ["Syrah"],
+    food: "Venison Chili with a Blueberry Reduction",
+    homepage: "https://spicyvines.com",
   },
   {
-    "name": "ACTA",
-    "address": "7505 Highway 128, Healdsburg",
-    "wines": ["Bordeaux Style Blends"],
-    "food": "Caramelized, Spiced & Savored Wood-Fired Flatbread",
-    "homepage": "https://actawinery.com"
+    name: "ACTA",
+    address: "7505 Highway 128, Healdsburg",
+    wines: ["Bordeaux Style Blends"],
+    food: "Caramelized, Spiced & Savored Wood-Fired Flatbread",
+    homepage: "https://actawinery.com",
   },
   {
-    "name": "Soda Rock Barn",
-    "address": "8015 Hwy 128, Healdsburg",
-    "wines": ["Bordeaux Style Blends"],
-    "food": "Braised Short Ribs & Creamy Polenta",
-    "homepage": "https://sodarockwinery.com"
+    name: "Soda Rock Barn",
+    address: "8015 Hwy 128, Healdsburg",
+    wines: ["Bordeaux Style Blends"],
+    food: "Braised Short Ribs & Creamy Polenta",
+    homepage: "https://sodarockwinery.com",
   },
   {
-    "name": "Alexander Valley Vineyards",
-    "address": "8644 Highway 128, Healdsburg",
-    "wines": ["Bordeaux Style Blends", "Rhone Style Blends", "Cabernet Sauvignon", "Merlot", "Syrah"],
-    "food": "Grilled Tri Tip with Roasted Cherry Tomato Salsa",
-    "homepage": "https://www.avvwine.com"
+    name: "Alexander Valley Vineyards",
+    address: "8644 Highway 128, Healdsburg",
+    wines: [
+      "Bordeaux Style Blends",
+      "Rhone Style Blends",
+      "Cabernet Sauvignon",
+      "Merlot",
+      "Syrah",
+    ],
+    food: "Grilled Tri Tip with Roasted Cherry Tomato Salsa",
+    homepage: "https://www.avvwine.com",
   },
   {
-    "name": "deLorimier Winery",
-    "address": "2001 Highway 128, Geyserville",
-    "wines": ["Bordeaux Style Blends"],
-    "food": "Beef Bourguignon with Mashed Potatoes and a mini–Herb Biscuit",
-    "homepage": "https://delorimierwinery.com"
+    name: "deLorimier Winery",
+    address: "2001 Highway 128, Geyserville",
+    wines: ["Bordeaux Style Blends"],
+    food: "Beef Bourguignon with Mashed Potatoes and a mini–Herb Biscuit",
+    homepage: "https://delorimierwinery.com",
   },
   {
-    "name": "Pedroncelli Winery",
-    "address": "1220 Canyon Road, Geyserville",
-    "wines": ["Bordeaux Style Blends"],
-    "food": "Polenta with Slow-Roasted Tomatoes and Teleme",
-    "homepage": "https://www.pedroncelli.com"
+    name: "Pedroncelli Winery",
+    address: "1220 Canyon Road, Geyserville",
+    wines: ["Bordeaux Style Blends"],
+    food: "Polenta with Slow-Roasted Tomatoes and Teleme",
+    homepage: "https://www.pedroncelli.com",
   },
   {
-    "name": "J. Rickards Winery",
-    "address": "24505 Chianti Road, Cloverdale",
-    "wines": ["Bordeaux Style Blends", "Rhone Style Blends", "Rhone Style Red Blend"],
-    "food": "Stuffed Pasta Shells with Butternut Squash and Béchamel",
-    "homepage": "https://www.jrwinery.com"
+    name: "J. Rickards Winery",
+    address: "24505 Chianti Road, Cloverdale",
+    wines: [
+      "Bordeaux Style Blends",
+      "Rhone Style Blends",
+      "Rhone Style Red Blend",
+    ],
+    food: "Stuffed Pasta Shells with Butternut Squash and Béchamel",
+    homepage: "https://www.jrwinery.com",
   },
   {
-    "name": "GC Lurton Vineyards",
-    "address": "422 Healdsburg Avenue, Healdsburg",
-    "wines": ["Bordeaux Style Blends"],
-    "food": "Beef Bourguignon Facon Grand Mère",
-    "homepage": "https://www.gclurton.com"
+    name: "GC Lurton Vineyards",
+    address: "422 Healdsburg Avenue, Healdsburg",
+    wines: ["Bordeaux Style Blends"],
+    food: "Beef Bourguignon Facon Grand Mère",
+    homepage: "https://www.gclurton.com",
   },
   {
-    "name": "EHRET Winery",
-    "address": "422 Healdsburg Avenue, Healdsburg",
-    "wines": ["Bordeaux Style Blends"],
-    "food": "Dark Chocolate Truffle",
-    "homepage": "https://ehretwinery.com"
-  }
+    name: "EHRET Winery",
+    address: "422 Healdsburg Avenue, Healdsburg",
+    wines: ["Bordeaux Style Blends"],
+    food: "Dark Chocolate Truffle",
+    homepage: "https://ehretwinery.com",
+  },
 ];
 
 export default wineries;
